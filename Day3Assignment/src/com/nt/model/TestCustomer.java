50c4b23eeaad5c3bf236c7599fda1187f1bf49bc
mahi <mahi@mahi-pc>
2021-04-15 23:46:04 +0545

    day3 Assignments

---
+++ Day3Assignment/src/com/nt/model/TestCustomer.java
@@ -0,0 +1,17 @@
+package com.nt.model;
+
+import com.nt.model.Address;
+import com.nt.model.Customer;
+ 
+
+public class TestCustomer {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Customer cust = new Customer("John", new Address("1st Main HSR Layout", "Bengaluru"), new Address("Ist Main Electronics city ","Bangalore"));
+		System.out.println(cust.getCustomerDetails());
+	}
+
+}
